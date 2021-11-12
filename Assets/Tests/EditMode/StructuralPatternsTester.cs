bc609e7960988557a25e417dbb51e325a9b23bb4
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-11-12 15:57:53 +0900

    NEW : BridgePattern 추가

--- Assets/Tests/EditMode/StructuralPatternsTester.cs
+++ Assets/Tests/EditMode/StructuralPatternsTester.cs
@@ -17,4 +17,24 @@ public class StructuralPatternsTester
         Debug.Log(mixer.Value);
         Assert.AreEqual(mixer.Value, 60f);
     }
+
+    [Test]
+    public void StructuralPatterns_BridgePattern()
+    {
+        var mortar = DesignPatterns.BridgePattern.Kitchen.GetInstance("Mortar");
+        var foodProcesser = DesignPatterns.BridgePattern.Kitchen.GetInstance("FoodProcesser");
+
+        mortar.Cook(5);
+        mortar.Cook(10);
+
+        foodProcesser.Cook(20f);
+        foodProcesser.Cook(10f);
+
+        Debug.Log(mortar.GetValue());
+        Debug.Log(foodProcesser.GetValue());
+        Assert.AreEqual(mortar.GetValue(), 15f);
+        Assert.AreEqual(foodProcesser.GetValue(), 30f);
+
+
+    }
 }
